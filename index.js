2b635f413cc66b6514af640b7549aa94f4fd7e15
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2023-12-06 04:17:00 +0400

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+fetch("https://dummyjson.com/products")
+  .then((data) => {
+    //whatever we write here it is gonna be converted to an object
+    return data.json();
+  })
+  .then((completedata) => {
+    // console.log(completedata);
+    // document.getElementById("root").innerHTML = completedata[2].title;
+    let data1 = [];
+    completedata.map((values) => {
+      data1 += ` 
+        <div class="productCard">
+          <h1 class="productTitle">${values.title}</h1>
+          <img src=${values.thumbnail} alt="img" class="productImage" />
+          <p>${values.description}</p>
+          <p class="productCategory">${values.category}</p>
+          <p class="productPrice">${values.price}</p>
+          <p class="productDiscount">${values.discountPercentage}</p>
+        </div>`;
+    });
+    document.getElementById("cards").innerHTML = data1;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
